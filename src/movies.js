f9a0fee72a9b915704b39389d4301a8b97f5c963
Gabriel <gabrifurquim@gmail.com>
2021-06-06 23:08:52 -0300

    trampo

--- src/movies.js
+++ src/movies.js
@@ -1,13 +1,37 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors() {}
+function getAllDirectors(listOfMovies) {
+  let directorsList = [];
+  for (i = 0; i < listOfMovies.length; i++) {
+    directorsList.push(listOfMovies[i].director);
+  }
+  return directorsList;
+  // return listOfMovies.map(directorsList.slice(listOfMovies.director));
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies() {}
+function howManyMovies(listOfMovies) {
+  let spielbergDrama = [];
+  for (i = 0; i < listOfMovies.length; i++) {
+    if (
+      (listOfMovies[i].director =
+        'Steven Spielberg' && listOfMovies[i].genre.includes('Drama'))
+    ) {
+      spielbergDrama.push(listOfMovies[i]);
+    }
+  }
+  return spielbergDrama.length;
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage() {}
+function scoresAverage(listOfMovies) {
+  sum = 0;
+  for (i = 0; i < listOfMovies.length; i++) {
+    sum += listOfMovies[i].score;
+  }
+  return sum / listOfMovies.length;
+}
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
 function dramaMoviesScore() {}
@@ -24,8 +48,6 @@ function turnHoursToMinutes() {}
 // BONUS - Iteration 8: Best yearly score average - Best yearly score average
 function bestYearAvg() {}
 
-
-
 // The following is required to make unit tests work.
 /* Environment setup. Do not modify the below code. */
 if (typeof module !== 'undefined') {
@@ -37,6 +59,6 @@ if (typeof module !== 'undefined') {
     orderByYear,
     orderAlphabetically,
     turnHoursToMinutes,
-    bestYearAvg,
+    bestYearAvg
   };
 }
